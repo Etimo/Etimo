c87e81e9b48d59bc144cdc933ca64031ac9166cc
Teo Klestrup Röijezon <teo@nullable.se>
2018-09-24 12:24:30 +0200

    Don't allow `useradd` and co

--- employee-users.nix
+++ employee-users.nix
@@ -2,9 +2,12 @@
 let
   employees = lib.importJSON ./employees.json;
 in {
-  users.users = lib.mapAttrs (name: employee: {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = employee.sshKeys;
-  }) employees;
+  users = {
+    mutableUsers = false;
+    users = lib.mapAttrs (name: employee: {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = employee.sshKeys;
+    }) employees;
+  };
 }
